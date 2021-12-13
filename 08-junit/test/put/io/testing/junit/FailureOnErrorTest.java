75cbf823671d7c10bc58d9326798f8ef55c49c74
Mateusz Olszewski <93004867+Ikswezslo@users.noreply.github.com>
2021-12-13 15:48:01 +0100

    Update FailureOnErrorTest.java

--- 08-junit/test/put/io/testing/junit/FailureOnErrorTest.java
+++ 08-junit/test/put/io/testing/junit/FailureOnErrorTest.java
@@ -5,12 +5,12 @@ import static org.junit.jupiter.api.Assertions.*;
 public class FailureOnErrorTest {
 
     @Test
-    void test1() { // Oznaczona jako Failure
+    void test1() { // Pytanie 4.1: Oznaczona jako Failure
         assertEquals(1, 2);
     }
 
     @Test
-    void test2() { // Oznaczona jako Error
+    void test2() { // Pytanie 4.1: Oznaczona jako Error
         throw new IllegalArgumentException();
     }
 
